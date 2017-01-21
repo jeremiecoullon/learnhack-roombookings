0e67ae8827a152d64d8be2b1583a9c6c4c9e5474
jeremie's mac <jeremie.coullon.12@ucl.ac.uk>
2017-01-21 18:18:47 +0000

    config fix

--- config.py
+++ config.py
@@ -13,14 +13,14 @@ CSV_DATA_PATH = os.path.join(basedir, 'CSV')
 
 
 # mail server settings
-MAIL_SERVER = 'smtp.googlemail.com'
-MAIL_PORT = 465
-MAIL_USE_TLS = False
-MAIL_USE_SSL = True
-with open('../RoomBookings_email_stuff.txt','r') as f:
-    email_stuff = f.read().split('\n')
-MAIL_USERNAME = email_stuff[0]
-MAIL_PASSWORD = email_stuff[1]
+# MAIL_SERVER = 'smtp.googlemail.com'
+# MAIL_PORT = 465
+# MAIL_USE_TLS = False
+# MAIL_USE_SSL = True
+# with open('../RoomBookings_email_stuff.txt','r') as f:
+#     email_stuff = f.read().split('\n')
+# MAIL_USERNAME = email_stuff[0]
+# MAIL_PASSWORD = email_stuff[1]
 
 # admin list
 ADMINS = ['jeremie.coullon@gmail.com']
